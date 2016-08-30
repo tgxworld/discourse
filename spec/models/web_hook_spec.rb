470611f9c5c7aa2a5583b1fa012b103d4fe55374
Guo Xiang Tan <tgx_world@hotmail.com>
2016-08-30 14:09:51 +0800

    Use `subject` in webhook model spec.

--- spec/models/web_hook_spec.rb
+++ spec/models/web_hook_spec.rb
@@ -7,30 +7,30 @@ describe WebHook do
   it { is_expected.to validate_presence_of :web_hook_event_types }
 
   describe '#content_types' do
-    before { @content_types = WebHook.content_types }
+    subject { WebHook.content_types }
 
     it "'json' (application/json) should be at 1st position" do
-      expect(@content_types['application/json']).to eq 1
+      expect(subject['application/json']).to eq(1)
     end
 
     it "'url_encoded' (application/x-www-form-urlencoded) should be at 2st position" do
-      expect(@content_types['application/x-www-form-urlencoded']).to eq 2
+      expect(subject['application/x-www-form-urlencoded']).to eq(2)
     end
   end
 
   describe '#last_delivery_statuses' do
-    before { @last_delivery_statuses = WebHook.last_delivery_statuses }
+    subject { WebHook.last_delivery_statuses }
 
     it "inactive should be at 1st position" do
-      expect(@last_delivery_statuses[:inactive]).to eq 1
+      expect(subject[:inactive]).to eq(1)
     end
 
     it "failed should be at 2st position" do
-      expect(@last_delivery_statuses[:failed]).to eq 2
+      expect(subject[:failed]).to eq(2)
     end
 
     it "successful should be at 3st position" do
-      expect(@last_delivery_statuses[:successful]).to eq 3
+      expect(subject[:successful]).to eq(3)
     end
   end
 
@@ -48,6 +48,7 @@ describe WebHook do
         post_hook.update_attributes!(active: false)
 
         expect(WebHook.find_by_type(:post)).to eq([])
+        expect(WebHook.find_by_type(:topic)).to eq([topic_hook])
       end
     end
 
