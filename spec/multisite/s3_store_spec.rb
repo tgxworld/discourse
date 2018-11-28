0073ccc626de2393dd5155aaddc64c0a8544b5aa
Rishabh <rishabh.nambiar@discourse.org>
2018-11-28 09:44:54 +0530

    Use File.join to concatenate all paths & fix linting on multisite/s3_store_spec.rb

--- spec/multisite/s3_store_spec.rb
+++ spec/multisite/s3_store_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Multisite s3 uploads', type: :multisite do
   let(:conn) { RailsMultisite::ConnectionManagement }
   let(:store) { FileStore::S3Store.new }
   let(:s3_helper) { store.instance_variable_get(:@s3_helper) }
-  let(:uploaded_file) { file_from_fixtures("logo.png") }
+  let(:uploaded_file) { file_from_fixtures("smallest.png") }
 
   before(:each) do
     SiteSetting.s3_upload_bucket = "s3-upload-bucket"
@@ -18,8 +18,8 @@ RSpec.describe 'Multisite s3 uploads', type: :multisite do
     let(:store) { FileStore::S3Store.new }
     let(:client) { Aws::S3::Client.new(stub_responses: true) }
     let(:resource) { Aws::S3::Resource.new(client: client) }
-    let(:s3_bucket) { resource.bucket("s3-upload-bucket") }
-    let(:s3_helper) { store.instance_variable_get(:@s3_helper) }
+    let(:s3_bucket) { resource.bucket(SiteSetting.s3_upload_bucket) }
+    let(:s3_helper) { store.s3_helper }
   end
 
   context 'uploading to s3' do
@@ -29,14 +29,14 @@ RSpec.describe 'Multisite s3 uploads', type: :multisite do
       it "returns the correct url for default multisite db" do
         s3_helper.expects(:s3_bucket).returns(s3_bucket).at_least_once
         s3_object = stub
-        upload = Fabricate(:upload, sha1: Digest::SHA1.hexdigest('secreet image string'))
+        upload = Fabricate(:upload, sha1: Digest::SHA1.hexdigest('secret image string'))
 
         conn.with_connection('default') do
           s3_bucket.expects(:object).with("original/1X/#{upload.sha1}.png").returns(s3_object)
           s3_object.expects(:upload_file)
 
           expect(store.store_upload(uploaded_file, upload)).to eq(
-          "//s3-upload-bucket.s3.dualstack.us-east-1.amazonaws.com/original/1X/#{upload.sha1}.png"
+            "//s3-upload-bucket.s3.dualstack.us-east-1.amazonaws.com/original/1X/#{upload.sha1}.png"
           )
         end
       end
@@ -44,14 +44,14 @@ RSpec.describe 'Multisite s3 uploads', type: :multisite do
       it "returns the correct url for second multisite db" do
         s3_helper.expects(:s3_bucket).returns(s3_bucket).at_least_once
         s3_object = stub
-        upload = Fabricate(:upload, sha1: Digest::SHA1.hexdigest('secreet second string'))
+        upload = Fabricate(:upload, sha1: Digest::SHA1.hexdigest('secret second string'))
 
         conn.with_connection('second') do
           s3_bucket.expects(:object).with("uploads/second/original/1X/#{upload.sha1}.png").returns(s3_object)
           s3_object.expects(:upload_file)
 
           expect(store.store_upload(uploaded_file, upload)).to eq(
-          "//s3-upload-bucket.s3.dualstack.us-east-1.amazonaws.com/uploads/second/original/1X/#{upload.sha1}.png"
+            "//s3-upload-bucket.s3.dualstack.us-east-1.amazonaws.com/uploads/second/original/1X/#{upload.sha1}.png"
           )
         end
       end
