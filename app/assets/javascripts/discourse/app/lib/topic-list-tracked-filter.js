431786d964a948d61e9f1043c4cf8bb783af2b5d
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2022-05-27 12:29:17 +0800

    FIX: Topic list nav items count not respecting tracked filter.
    This commit seeks to only handle the `f=tracked` and `filter=tracked`
    query params for a topic list. There are other "hidden" filters for a
    topic list which can be activated by passing the right query param to
    the request. However, they are hidden because there is no way to
    activate those filters via the UI. We are handling the `f=tracked`
    filter because we will soon be adding a link that allows a user to
    quickly view their tracked topics.

---
+++ app/assets/javascripts/discourse/app/lib/topic-list-tracked-filter.js
@@ -0,0 +1,45 @@
+import Site from "discourse/models/site";
+import User from "discourse/models/user";
+
+export function hasTrackedFilter(queryParams) {
+  if (!queryParams) {
+    return false;
+  }
+
+  return queryParams.f === "tracked" || queryParams.filter === "tracked";
+}
+
+/**
+ * Logic here needs to be in sync with `TopicQuery#tracked_filter` on the server side. See `TopicQuery#tracked_filter`
+ * or the rational behind this decision.
+ */
+export function isTrackedTopic(topic) {
+  if (topic.category_id) {
+    const categories = Site.current().trackedCategoriesList;
+
+    for (const category of categories) {
+      if (category.id === topic.category_id) {
+        return true;
+      }
+
+      if (
+        category.subcategories &&
+        category.subcategories.some((c) => c.id === topic.category_id)
+      ) {
+        return true;
+      }
+    }
+  }
+
+  if (topic.tags) {
+    const tags = User.current().trackedTags;
+
+    for (const tag of tags) {
+      if (topic.tags.indexOf(tag) > -1) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
